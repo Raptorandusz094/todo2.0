7407bf63a0dbaf4851657ec765982afaf0642aa9
Dan Andras <danandras94@gmail.com>
2023-09-21 13:18:52 +0200

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./app/**/*.liquid"],
   theme: {
-    extend: {},
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px '
+    },
+    extend: {
+      colors:
+      {brightPurple:'hsl(265, 98%, 67%)',
+      brightPurpleLight:'hsl(265, 100%, 86%)',
+      brightPurpleSupLight:'hsl(266, 92%, 95%)',
+      darkBlue:'hsl(228,39%,23%)',
+     dargGraylishBlue:'hsl(227,12%,61%)',
+      veryDarkBlue:'hsl(233,12%,13%)',
+      veryPaleRed:'hsl(13,100%,96%)',
+      veryLightGray:'hsl(0,0,98%)',
+    }
+
+
+    },
   },
   plugins: [],
 }
